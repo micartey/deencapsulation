c640449f248895c37f26e1d3b16c586a238a0bee
micartey <me@micartey.dev>
2025-06-23 17:16:12 +0200

    first try of allowing native bypass

--- src/main/java/dev/xdark/deencapsulation/Deencapsulation.java
+++ src/main/java/dev/xdark/deencapsulation/Deencapsulation.java
@@ -6,44 +6,53 @@ import java.lang.invoke.MethodHandle;
 import java.lang.invoke.MethodHandles;
 import java.lang.invoke.MethodType;
 import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
 import java.util.HashSet;
 import java.util.Set;
 
 public final class Deencapsulation {
 
-	private Deencapsulation() {
-	}
-
-	public static MethodHandles.Lookup lookup(Class<?> lookupClass) {
+	private static MethodHandles.Lookup lookup(Class<?> lookupClass) {
 		try {
-			Constructor<?> ctor = ReflectionFactory.getReflectionFactory()
-					.newConstructorForSerialization(MethodHandles.Lookup.class, MethodHandles.Lookup.class.getDeclaredConstructor(Class.class));
-			return (MethodHandles.Lookup) ctor.newInstance(lookupClass);
+			Constructor<?> constructor = ReflectionFactory.getReflectionFactory()
+					.newConstructorForSerialization(
+                            MethodHandles.Lookup.class,
+                            MethodHandles.Lookup.class.getDeclaredConstructor(Class.class)
+                    );
+
+			return (MethodHandles.Lookup) constructor.newInstance(lookupClass);
 		} catch (ReflectiveOperationException e) {
-			Throwable t;
-			if (e instanceof InvocationTargetException) {
-				t = ((InvocationTargetException) e).getTargetException();
-			} else {
-				t = e;
-			}
-			throw new IllegalStateException(t);
+			throw new IllegalStateException("That should have not happened", e);
 		}
 	}
+
+    private static Set<Module> getModules(Class<?> caller) {
+        Set<Module> modules = new HashSet<>();
+        Module base = caller.getModule();
+        ModuleLayer baseLayer = base.getLayer();
+
+        if (baseLayer != null)
+            modules.addAll(baseLayer.modules());
+
+        modules.addAll(ModuleLayer.boot().modules());
+
+        for (ClassLoader cl = caller.getClassLoader(); cl != null; cl = cl.getParent()) {
+            modules.add(cl.getUnnamedModule());
+        }
+
+        return modules;
+    }
 
 	public static void deencapsulate(Class<?> caller) {
-		Set<Module> modules = new HashSet<>();
-		Module base = caller.getModule();
-		ModuleLayer baseLayer = base.getLayer();
-		if (baseLayer != null)
-			modules.addAll(baseLayer.modules());
-		modules.addAll(ModuleLayer.boot().modules());
-		for (ClassLoader cl = caller.getClassLoader(); cl != null; cl = cl.getParent()) {
-			modules.add(cl.getUnnamedModule());
-		}
-		try {
-			MethodHandle export = lookup(Module.class).findVirtual(Module.class, "implAddOpens", MethodType.methodType(void.class, String.class));
-			for (Module module : modules) {
+        Set<Module> modules = getModules(caller);
+        try {
+			MethodHandle export = lookup(Module.class)
+                    .findVirtual(
+                            Module.class,
+                            "implAddOpens",
+                            MethodType.methodType(void.class, String.class)
+                    );
+
+            for (Module module : modules) {
 				for (String name : module.getPackages()) {
 					export.invokeExact(module, name);
 				}
@@ -52,4 +61,22 @@ public final class Deencapsulation {
 			throw new IllegalStateException("Could not export packages", t);
 		}
 	}
+
+    public static void allowNative(Class<?> caller) {
+        Set<Module> modules = getModules(caller);
+        try {
+            MethodHandle export = lookup(Module.class)
+                    .findVirtual(
+                            Module.class,
+                            "implAddEnableNativeAccess",
+                            MethodType.methodType(Module.class)
+                    );
+
+            for (Module module : modules) {
+                export.invokeExact(module);
+            }
+        } catch (Throwable t) {
+            throw new IllegalStateException("Could not export packages", t);
+        }
+    }
 }
